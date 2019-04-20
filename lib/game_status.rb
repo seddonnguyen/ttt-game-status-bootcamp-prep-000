affbf799a927bff33a95bfce842ebea44257b96d
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-20 18:48:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,7 @@ def won?(board)
     pos_2 = board[idx_2]
     pos_3 = board[idx_3]
 
-    if pos_1 == pos_2 && pos_1 == pos_3
+    if pos_1 == pos_2 && pos_1 == pos_3 && pos_1 != " "
       return combo
     else
       false
